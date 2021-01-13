132fdf11528f783efea61f77493f828403747960
Veena Badagu <vbadagu1@binghamton.edu>
2021-01-13 10:07:08 +0530

    Test code

---
+++ src/MyClass.java
@@ -0,0 +1,11 @@
+public class MyClass {
+    public static void main(String[] args)
+    {
+        int example = 4;
+        System.out.println("Hiii Sweeetieeeeeeeeeee");
+        example = example *4;
+        int y = 42;
+        example = example / y;
+
+    }
+}
